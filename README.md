4d3cb46c10633591410018b064fcc73eeb2a23b8
Paul Kilmurray <paul@kilbot.com>
2024-06-20 19:38:30 +0100

    update naming

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## WCPOS Email Invoice
 
-1. Check [releases](https://github.com/kilbot/wcpos-email-invoice/releases) for the latest version of the plugin.
-2. Download the **wcpos-email-invoice.zip** file.
-3. Install & activate the plugin via WP Admin > Plugins > Add New > Upload Plugin.
-4. Go to WP Admin > WooCommerce POS > Settings > Checkout > enable the WooCommerce POS Invoice Payment Gateway.
+1. Check [releases](https://github.com/wcpos/email-invoice-gateway/releases) for the latest version of the plugin.
+2. Download the **email-invoice-gateway.zip** file.
+3. Install & activate the plugin via `WP Admin > Plugins > Add New > Upload Plugin`.
+4. Go to `WP Admin > WooCommerce POS > Settings > Checkout > enable` the WooCommerce POS Invoice Payment Gateway.
--- wcpos-email-invoice.php
+++ wcpos-email-invoice.php
@@ -1,109 +1,110 @@
 <?php
 /**
- * Plugin Name: WooCommerce POS Invoice Payment Gateway
- * Plugin URI: https://github.com/kilbot/wcpos-email-invoice
- * Description: Send an email to the customer with a link to pay for the order.
- * Version: 0.0.4
+ * Plugin Name: WooCommerce POS Email Invoice Gateway
+ * Plugin URI: https://github.com/wcpos/email-invoice-gateway
+ * Description: Send an invoice email to the customer with a link to pay for the order.
+ * Version: 0.0.5
  * Author: kilbot
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain: wcpos-email-invoice
+ * Text Domain: woocommerce-pos-email-invoice-gateway
  */
 
-add_action('plugins_loaded', 'woocommerce_custom_gateway_init', 0);
+add_action( 'plugins_loaded', 'woocommerce_pos_email_invoice_gateway_init', 0 );
 
-function woocommerce_custom_gateway_init() {
-    if (!class_exists('WC_Payment_Gateway')) return;
+function woocommerce_pos_email_invoice_gateway_init() {
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		return;
+	}
 
-    /**
-     * Localisation
-     */
-    load_plugin_textdomain('wcpos-email-invoice', false, dirname(plugin_basename(__FILE__)) . '/languages');
-    
-    /**
-     * Gateway class
-     */
-    class WCPOS_Email_Invoice extends WC_Payment_Gateway {
+	/**
+	 * Localisation
+	 */
+	load_plugin_textdomain( 'woocommerce-pos-email-invoice-gateway', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 
-        public function __construct() {
-            $this->id = 'pos_email_invoice';
-            $this->icon = ''; // URL of the icon that will be displayed on checkout page near your gateway name
-            $this->has_fields = false;
-            $this->method_title = 'Custom Gateway';
-            $this->method_description = 'Description of your custom gateway';
+	/**
+	 * Gateway class
+	 */
+	class WCPOS_Email_Invoice extends WC_Payment_Gateway {
 
-            // Load the settings.
-            $this->init_form_fields();
-            $this->init_settings();
+		public function __construct() {
+			$this->id                 = 'wcpos_email_invoice';
+			$this->icon               = '';
+			$this->has_fields         = false;
+			$this->method_title       = 'Email Invoice Gateway';
+			$this->method_description = 'Send an invoice email to the customer with a link to pay for the order.';
 
-            // Define user set variables
-            $this->title = $this->get_option('title');
-            $this->description = $this->get_option('description');
-            $this->instructions = $this->get_option('instructions', $this->description);
+			// Load the settings.
+			$this->init_form_fields();
+			$this->init_settings();
 
-            // only allow in the POS
-            $this->enabled = false;
+			// Define user set variables
+			$this->title       = $this->get_option( 'title' );
+			$this->description = $this->get_option( 'description' );
 
-            // Actions
-            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
-        }
+			// only allow in the POS
+			$this->enabled = false;
 
-        public function init_form_fields() {
-            $this->form_fields = array(
-                'title' => array(
-                    'title' => __('Title', 'wcpos-email-invoice'),
-                    'type' => 'text',
-                    'description' => __('This controls the title which the user sees during checkout.', 'wcpos-email-invoice'),
-                    'default' => __('Send Invoice Email', 'wcpos-email-invoice'),
-                    'desc_tip' => true,
-                ),
-                'description' => array(
-                    'title' => __('Description', 'wcpos-email-invoice'),
-                    'type' => 'textarea',
-                    'description' => __('This controls the description which the user sees during checkout.', 'wcpos-email-invoice'),
-                    'default' => __('An email will be sent to the customer email with \'Pay for this order\' link.', 'wcpos-email-invoice')
-                )
-            );
-        }
+			// Actions
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		}
 
-        public function process_payment($order_id) {
-          $order = wc_get_order($order_id);
-      
-          // Mark as pending (we're awaiting the payment)
-          $order->update_status('pending', __('Awaiting custom payment. ', 'wc-custom-gateway'));
-      
-          // Reduce stock levels
-          wc_reduce_stock_levels($order_id);
-      
-          // Trigger the 'email customer invoice' action
-          $mailer = WC()->mailer();
-          $email_to_send = 'WC_Email_Customer_Invoice';
-          $emails = $mailer->get_emails();
-      
-          // Ensure the email class exists and send the email
-          if (!empty($emails) && isset($emails[$email_to_send])) {
-            $emails[$email_to_send]->trigger($order->get_id(), $order);
-            $order->add_order_note(__('Customer invoice email sent via POS. ', 'wc-custom-gateway'));
-          } else {
-            $order->add_order_note(__('Failed to send customer invoice email. ', 'wc-custom-gateway'));
-          }
-      
-          // Return thankyou redirect
-          return array(
-              'result' => 'success',
-              'redirect' => $this->get_return_url($order)
-          );
-      }
-      
-    }
+		public function init_form_fields() {
+			$this->form_fields = array(
+				'title'       => array(
+					'title'       => __( 'Title', 'wcpos-email-invoice' ),
+					'type'        => 'text',
+					'description' => __( 'This controls the title which the user sees during checkout.', 'wcpos-email-invoice' ),
+					'default'     => __( 'Send Invoice Email', 'wcpos-email-invoice' ),
+					'desc_tip'    => true,
+				),
+				'description' => array(
+					'title'       => __( 'Description', 'wcpos-email-invoice' ),
+					'type'        => 'textarea',
+					'description' => __( 'This controls the description which the user sees during checkout.', 'wcpos-email-invoice' ),
+					'default'     => __( 'An email will be sent to the customer email with \'Pay for this order\' link.', 'wcpos-email-invoice' ),
+				),
+			);
+		}
 
-    /**
-     * Add the Gateway to WooCommerce
-     **/
-    function woocommerce_add_custom_gateway($methods) {
-        $methods[] = 'WCPOS_Email_Invoice';
-        return $methods;
-    }
+		public function process_payment( $order_id ) {
+			$order = wc_get_order( $order_id );
 
-    add_filter('woocommerce_payment_gateways', 'woocommerce_add_custom_gateway');
+			// Mark as pending (we're awaiting the payment)
+			$order->update_status( 'pending', __( 'Awaiting custom payment. ', 'woocommerce-pos-email-invoice-gateway' ) );
+
+			// Reduce stock levels
+			wc_reduce_stock_levels( $order_id );
+
+			// Trigger the 'email customer invoice' action
+			$mailer        = WC()->mailer();
+			$email_to_send = 'WC_Email_Customer_Invoice';
+			$emails        = $mailer->get_emails();
+
+			// Ensure the email class exists and send the email
+			if ( ! empty( $emails ) && isset( $emails[ $email_to_send ] ) ) {
+				$emails[ $email_to_send ]->trigger( $order->get_id(), $order );
+				$order->add_order_note( __( 'Customer invoice email sent via POS. ', 'woocommerce-pos-email-invoice-gateway' ) );
+			} else {
+				$order->add_order_note( __( 'Failed to send customer invoice email. ', 'woocommerce-pos-email-invoice-gateway' ) );
+			}
+
+			// Return thankyou redirect
+			return array(
+				'result'   => 'success',
+				'redirect' => $this->get_return_url( $order ),
+			);
+		}
+	}
+
+	/**
+	 * Add the Gateway to WooCommerce
+	 */
+	add_filter(
+		'woocommerce_payment_gateways',
+		function ( $methods ) {
+			$methods[] = 'WCPOS_Email_Invoice';
+			return $methods;
+		}
+	);
 }
